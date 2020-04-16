da8a6a23a5c356ee7c1aede67286830d5b64ea65
muhammed kılınç <muhammed.kilinc@inomera.com>
2020-04-16 10:30:36 +0300

    Initial Commit

---
+++ NetmeraSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetmeraSample
+//
+//  Created by inomera on 16.04.2020.
+//  Copyright © 2020 Muhammed Kılınç. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
